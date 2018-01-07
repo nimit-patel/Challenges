5439fb0e4b27dd62467c14de3ff6316149c5e65f
npatel52 <nimit444@gmail.com>
2018-01-06 22:06:42 -0500

    Similar but avoids overhead in doing arithmetic

--- LLS/solution.java
+++ LLS/solution.java
@@ -1,34 +1,31 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // Common cases
-        if(s == null || s == ""){
-            return 0;
+
+      int len = s.length();
+      Set<Character> set = new HashSet<>();
+      int result = 0, start = 0, end = 0;
+
+      /* sliding window :
+        abca
+        0: a end:1 start:0 max:1
+        1: b end:2 start:0 max:2
+        2: c end:3 start:0 max:3
+        3: a => removes a end: 3 start:1 max:3
+        3: a is not in the set so add a. end:4 start:1 max:3
+        return max
+      */
+      while(start < len && end < len){
+
+        if(!set.contains(s.charAt(end))){
+          set.add(s.charAt(end++));
+          result = Math.max(result, end - start);
+        }else{
+          set.remove(s.charAt(start++));
         }
-        
-        if(s.length() == 1){
-            return 1;
-        }
-        
-        int max = 0;
-        HashSet<Character> uChars = new HashSet<Character>();
-        char matchedChar = ' ';
-        
-        for(int i = 0; i < s.length() && max < (s.length() - i + uChars.size()); ++i){ 
-                if(!uChars.add(s.charAt(i))){
-                    max = Math.max(max, uChars.size()); 
-                    int startIndex = i - uChars.size();
-                    int indexOfChar = s.indexOf(s.charAt(i), startIndex); // first occurence of this char
-                    
-                    for(;startIndex <= indexOfChar; ++startIndex){ // removal of all extra chars
-                        uChars.remove(s.charAt(startIndex)); 
-                    }
-                    uChars.add(s.charAt(i)); // adding this matched char to the set
-                }         
-        }
-        
-         // important to store max in case the inner loop reaches end of the string
-        max = Math.max(max, uChars.size());     
-        
-        return max;
+
+      }
+
+      return result;
     }
+
 }
